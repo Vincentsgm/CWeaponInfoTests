678e9b8bd4297bb0c18f5261f6ad027311b00393
Vincent angot <angosyl@gmail.com>
2023-04-24 23:30:34 +0200

    Sync

---
+++ CPed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWeaponInfoTests
+{
+    [StructLayout(LayoutKind.Explicit)]
+    internal unsafe struct CPed
+    {
+        [FieldOffset(0x10D8)] public CPedWeaponManager* weaponManager;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal unsafe struct CPedWeaponManager
+    {
+        [FieldOffset(0x020)] public CWeaponInfo* weaponInfo;
+    }
+}
--- EntryPoint.cs
+++ EntryPoint.cs
@@ -35,21 +35,40 @@ namespace CWeaponInfoTests
             Weapons = (atArrayOfPtrs<CWeaponInfo>*)WeaponsAddress;
             Game.LogTrivial($"{Weapons->Count}");
 
-            for (CWeaponInfo** weapon = Weapons->Items; weapon < Weapons->Items + Weapons->Count; weapon++)
+            for (int i = 0; i < Weapons->Count; i++)
             {
-                CWeaponInfo* wp = *weapon;
+                CWeaponInfo* wp = Weapons->Items[i];
                 if (wp->Name == (uint)WeaponHash.CarbineRifle)
                 {
                     Game.LogTrivial($"Carbine rifle found");
                     CarbineRifle = wp;
                 }
             }
-            Game.LogTrivial($"Carbine rifle time between shots: {CarbineRifle->BulletsPerAnimLoop}");
-            CarbineRifle->BulletsPerAnimLoop = 1;
-            Game.LogTrivial($"Carbine rifle time between shots: {CarbineRifle->BulletsPerAnimLoop}");
+            Game.LogTrivial($"Carbine rifle flags 1: {CarbineRifle->WeaponFlags1}");
+            Game.LogTrivial($"Carbine rifle flags 2: {CarbineRifle->WeaponFlags2}");
+            Game.LogTrivial($"Carbine rifle flags 3: {CarbineRifle->WeaponFlags3}");
+            SetWeaponFlag(CarbineRifle, eWeaponFlags1.OnlyFireOneShot, false);
+            SetWeaponFlag(CarbineRifle, eWeaponFlags1.Automatic, true);
+            SetWeaponFlag(CarbineRifle, eWeaponFlags1.OnlyFireOneShotPerTriggerPress, false);
+
+            if (Game.LocalPlayer.Character.Inventory.EquippedWeapon == null) return;
+            CPed* ped = (CPed*)Game.LocalPlayer.Character.MemoryAddress;
+            CWeaponInfo* playerGun = ped->weaponManager->weaponInfo;
+            Game.LogTrivial($"Player's weapon hash: {(WeaponHash)playerGun->Name}");
+            Game.LogTrivial($"Address are the same: {(IntPtr)playerGun == (IntPtr)CarbineRifle}");
+            /*CWeaponInfo carbine = *playerGun;
+            carbine.WeaponFlags1 |= eWeaponFlags1.OnlyFireOneShotPerTriggerPress;
+            ped->weaponManager->weaponInfo = &carbine;*/
+            
+
+
             while (true)
             {
                 GameFiber.Yield();
+                if (Game.LocalPlayer.Character.Inventory.EquippedWeapon == null) continue;
+                CWeaponInfo* newPlayerGun = ped->weaponManager->weaponInfo;
+                SetWeaponFlag(newPlayerGun, eWeaponFlags1.OnlyFireOneShotPerTriggerPress, true);
+                Game.DisplaySubtitle($"Address are the same: {(IntPtr)newPlayerGun == (IntPtr)CarbineRifle}");
                 if (Game.IsKeyDown(Keys.End)) Game.UnloadActivePlugin();
             }
         }
@@ -57,6 +76,24 @@ namespace CWeaponInfoTests
         public static void Exit()
         {
             CarbineRifle->Speed = OriginalTimeBetweenShots;
+        }
+
+        public static void SetWeaponFlag(CWeaponInfo* weapon, eWeaponFlags1 flag, bool enable)
+        {
+            if (enable) weapon->WeaponFlags1 |= flag;
+            else weapon->WeaponFlags1 &= ~flag;
+        }
+
+        public static void SetWeaponFlag(CWeaponInfo* weapon, eWeaponFlags2 flag, bool enable)
+        {
+            if (enable) weapon->WeaponFlags2 |= flag;
+            else weapon->WeaponFlags2 &= ~flag;
+        }
+
+        public static void SetWeaponFlag(CWeaponInfo* weapon, eWeaponFlags3 flag, bool enable)
+        {
+            if (enable) weapon->WeaponFlags3 |= flag;
+            else weapon->WeaponFlags3 &= ~flag;
         }
 
         public static void ShowWeaponFXGroups()
